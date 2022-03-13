13425a81a496ae7773a82149c22da19ecc6ee340
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2022-03-13 18:22:03 +0100

    Add 'share/config.sh'
    Signed-off-by: Murdo B. Maclachlan <murdomaclachlan@duck.com>

---
+++ share/config.sh
@@ -0,0 +1,9 @@
+# CONFIGURATION OPTIONS FOR INSTALLSP
+
+# For folder locations, ensure you have a slash at the end or
+# installsp may fail.
+
+# Location of local repository
+repository="/var/cache/repository/"
+# Location to look for package
+sploc="/tmp/"
