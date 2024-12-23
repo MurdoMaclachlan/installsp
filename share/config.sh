76e34ee6685122262c4f54efba343c9171139828
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2024-12-23 18:19:31 +0000

    Release 1.0.0

--- share/config.sh
+++ share/config.sh
@@ -1,9 +1,7 @@
 # CONFIGURATION OPTIONS FOR INSTALLSP
 
-# For folder locations, ensure you have a slash at the end or
-# installsp may fail.
+# Location of local repository; leave empty to instruct installsp not to move packages.
+REPOSITORY="/var/cache/repository"
 
-# Location of local repository
-repository="/var/cache/repository/"
-# Location to look for package
-sploc="/tmp/"
+# Location to look for package.
+PACKAGE_LOCATION="/tmp"
